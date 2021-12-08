38c4dc655ed753b0020c4a68eb7e598f14ed8910
han <gksrbgks2021@gmail.com>
2021-12-09 00:13:57 +0900

    이동애니메이션구현

--- src/Stickman.java
+++ src/Stickman.java
@@ -8,87 +8,143 @@ class Stickman extends JLabel {
 	// 좌표.
 	private int x;
 	private int y;
-
+	private int height;
 	private boolean Up;
 	private boolean Down;
 	private boolean Right;
 	private boolean Left;
 	private boolean goDown;
+	private int stickanime;
 	private int jumpSpeed;
+	private int jumpstart;
 	private int status; // 현재 상태. 1이면 기본., . 2면 움크리기. 3이면 오른쪽 진행, 4면 왼쪽.
 //속도 상태 
-	private ImageIcon manUp, manDown;
+	private ImageIcon[] manUp;
+	private ImageIcon manDown;
+	private ImageIcon manJump;
 	private ImageIcon manleft1, manleft2;
 	private ImageIcon manright1, manright2;
-	private Timer t1, t2;
+	private Timer t[];
 
 	public Stickman() {
 		init();
 		setlabel();
-		
 	}
 
 	public void init() {
 		// 초기화 합니다.
 		x = 54;
-		y = 453;
+		y = 453; // 바닥은 593
 		Up = false;
 		Down = false;
 		goDown = false;
-		jumpSpeed = 10;
+		jumpSpeed = 1;
+		jumpstart = -30;
 		status = 1;
+		t = new Timer[4];
+		stickanime = 0;
 	}
 
 	public void setlabel() {
 
-		manUp = new ImageIcon(Main.class.getResource("images/Player2.png"));
+		manUp = new ImageIcon[5];
+		manUp[0] = new ImageIcon(Main.class.getResource("images/Player2.png"));
+		manUp[1] = new ImageIcon(Main.class.getResource("images/Player2_1.png"));
+		manUp[2] = new ImageIcon(Main.class.getResource("images/Player2_2.png"));
+		manUp[3] = new ImageIcon(Main.class.getResource("images/Player2_3.png"));
+		manUp[4] = new ImageIcon(Main.class.getResource("images/Player2_4.png"));
 		manDown = new ImageIcon(Main.class.getResource("images/playerdown1.png"));
+		manJump = new ImageIcon(Main.class.getResource("images/jump_1.png"));
 		manleft1 = new ImageIcon(Main.class.getResource("images/Player2_l.png"));
-
-		setSize(70, 140);
-		setIcon(manUp);
+		height = 140;
+		setSize(70, height);
+		setIcon(manUp[stickanime]);
 		setLocation(x, y);
 
-		t1 = new Timer(30, e -> { // t1은 오른쪽으로 이동하는거다.
-			if(getX() >=1210)
-				t1.stop();
+		t[0] = new Timer(30, e -> { // t1은 오른쪽으로 이동하는거다.
+			if (getX() >= 1210)
+				t[0].stop();
 			x += 5;
 			setLocation(x, y);
 		});
-		
-		t2 = new Timer(30, e -> { // t1은 왼쪽으로 이동하는거다.
-			if(getX() <=0)
-				t2.stop();
+
+		t[1] = new Timer(30, e -> { // t1은 왼쪽으로 이동하는거다.
+			if (getX() <= 0)
+				t[1].stop();
 			x -= 5;
 			setLocation(x, y);
 		});
+
+		t[2] = new Timer(50, e -> {
+			y += jumpstart;
+			jumpstart += jumpSpeed;
+			checkfloor();
+			setLocation(x, y);
+		});
+		t[3] = new Timer(50, e -> {
+			if (status == 1 && !Up)// 일어나있는 상태이면
+				setIcon(manUp[(stickanime++) % 5]);
+			if (stickanime == Integer.MAX_VALUE)// 충돌방지.
+				stickanime = 0;
+		});
+	}
+
+	public void runStickman() { // 움직임 구현.
+		t[3].start();
+	}
+
+	public void stopStickman() { // 움직임 구현.
+		t[3].stop();
+	}
+
+	public void checkfloor() {// 바닥인지아닌지.
+
+		if (getY() + height >= 593) {
+			setLocation(x, getY() + height);
+			Up = false;
+			t[2].stop();
+		}
+	}
+
+	public void Up() {
+		if (!Up) {
+			jumpSpeed = 10;
+			jumpstart = -50;
+			Up = true;
+			if (!Down)
+				setIcon(manJump);
+			t[2].start();
+		}
 	}
 
 	// jump 한다.
-	public void Up() {
-		Up = true;
-		// 한번 점프하면
-		new Thread(() -> {
-			try {
-				// y좌표 80만큼 올라갔다 내려온다.
-				for (int i = 0; i < 130 / jumpSpeed; i++) {
-					y = y - jumpSpeed;
-					setLocation(x, y);
-					Thread.sleep(60);
-				}
-				Up = false;
-				goDown();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}).start();
+//	public void Up() {
+//		Up = true;
+//		// 한번 점프하면
+//		new Thread(() -> {
+//			try {
+//				// y좌표 80만큼 올라갔다 내려온다.
+//				for (int i = 0; i < 180 / jumpSpeed; i++) {
+//					y = y - jumpSpeed;
+//					setLocation(x, y);
+//					Thread.sleep(60);
+//				}
+//				Up = false;
+//				goDown();
+//			} catch (Exception e) {
+//				e.printStackTrace();
+//			}
+//		}).start();
+//	}
+	public boolean a() {
+		return Up;
 	}
 
 	public void goDown() {
 		goDown = true;
 		new Thread(() -> {
 			try {
-				for (int i = 0; i < 130 / jumpSpeed; i++) {
+				for (int i = 0; i < 180 / jumpSpeed; i++) {
 					y = y + jumpSpeed;
 					setLocation(x, y);
 					Thread.sleep(60);
@@ -99,15 +155,16 @@ class Stickman extends JLabel {
 				e.printStackTrace();
 			}
 		}).start();
-
 	}
 
 	public void Down() {
 		// 엎드린 상태 일때 그리는 좌표, 사이즈 최신화
 		if (Down == false && status == 1) {
-			setSize(70, 70);
+			height = 70;
+			setSize(70, height);
 			this.y += 70;
 			status = 2;// 엎드린 상태.
+			stopStickman();
 		}
 		Down = true;
 		setIcon(manDown);
@@ -120,49 +177,53 @@ class Stickman extends JLabel {
 //			status = 3;
 //			Right = true;
 //		}
-		if(!Right) {
+		if (!Right) {
 			Right = true;
-			if(getX() <=1210 ) {
-				setIcon(manUp);
-				t1.start();	
-			}	
+			if (getX() <= 1210) {
+				t[0].start();
+			}
 		}
 	}
-	
+
 	public void Left() {
 //		if (Left == false) {
 //			this.x += 70;
 //			status = 3;
 //			Right = true;
 //		}
-		if(!Left) {
+		if (!Left) {
 			Left = true;
-			if(getX() >= 0) {
-				setIcon(manUp);
-				t2.start();	
-			}	
+			if (getX() >= 0) {
+				t[1].start();
+			}
 		}
 	}
-	
+
 	public void wakeUp() {
 		if (status == 2) {
-			setSize(70, 140);
+			height = 140;
+			setSize(70, height);
 			this.y -= 70;
 			status = 1;
+			this.runStickman();
+			Down = false;
 		}
-		Down = false;
-		setIcon(manUp);
+		if (Up) {
+			Down = false;
+			setIcon(manJump);
+		}
+
 		setLocation(x, y);
 	}
 
 	public void stop_R() {
 		Right = false;
-		t1.stop();
+		t[0].stop();
 	}
 
 	public void stop_L() {
 		Left = false;
-		t2.stop();
+		t[1].stop();
 	}
 
 	public boolean isUp() {
@@ -171,6 +232,10 @@ class Stickman extends JLabel {
 
 	public boolean isgoDown() {
 		return this.goDown;
+	}
+
+	public int height() { // 1이면 기본, 2면 엎드린거.
+		return height;
 	}
 
 	public int getX() {
